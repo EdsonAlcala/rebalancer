89e79ebef32fc6ab0c37d520bed23589e7a8d66d
edsonalcala <edson_alcala@msn.com>
2025-12-18 16:28:04 +0100

    added allowances functions

---
+++ agent/src/adapters/rebalancer_contract/allowances/__init__.py
@@ -0,0 +1,9 @@
+from .approve_aave import ApproveAave
+from .approve_messenger import ApproveMessengerCctp
+from .approve_vault import ApproveVault
+
+__all__ = [
+    "ApproveAave",
+    "ApproveMessengerCctp",
+    "ApproveVault",
+]
