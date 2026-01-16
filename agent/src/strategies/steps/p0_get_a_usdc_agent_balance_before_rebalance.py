d1ac5cadb20a58ae3009bfc977a4d086938f8a25
edsonalcala <edson_alcala@msn.com>
2026-01-16 16:54:17 +0100

    fixed problem with a token address

--- agent/src/strategies/steps/p0_get_a_usdc_agent_balance_before_rebalance.py
+++ agent/src/strategies/steps/p0_get_a_usdc_agent_balance_before_rebalance.py
@@ -11,6 +11,16 @@ class GetAUSDCBalanceBeforeRebalance(Step):
     async def run(self, ctx: StrategyContext) -> None:
         print("Getting AUSDC agent balance before rebalance...")
         
+        ctx.a_token_address_on_destination_chain = LendingPool.get_atoken_address(ctx.web3_destination, ctx.aave_lending_pool_address_on_destination_chain, ctx.usdc_token_address_on_destination_chain)
+
+        if ctx.a_token_address_on_destination_chain is None:
+            raise ValueError("a_token_address_on_destination_chain is not set in context.")
+        
+        ctx.a_token_address_on_source_chain = LendingPool.get_atoken_address(ctx.web3_source, ctx.aave_lending_pool_address_on_source_chain, ctx.usdc_token_address_on_source_chain)
+
+        if ctx.a_token_address_on_source_chain is None:
+            raise ValueError("a_token_address_on_source_chain is not set in context.")
+   
         if ctx.is_restart:
             print("⚠️ Restart detected; skipping fetching AUSDC balance before rebalance.")
 
@@ -22,16 +32,6 @@ class GetAUSDCBalanceBeforeRebalance(Step):
             
             return
         
-        ctx.a_token_address_on_destination_chain = LendingPool.get_atoken_address(ctx.web3_destination, ctx.aave_lending_pool_address_on_destination_chain, ctx.usdc_token_address_on_destination_chain)
-
-        if ctx.a_token_address_on_destination_chain is None:
-            raise ValueError("a_token_address_on_destination_chain is not set in context.")
-        
-        ctx.a_token_address_on_source_chain = LendingPool.get_atoken_address(ctx.web3_source, ctx.aave_lending_pool_address_on_source_chain, ctx.usdc_token_address_on_source_chain)
-
-        if ctx.a_token_address_on_source_chain is None:
-            raise ValueError("a_token_address_on_source_chain is not set in context.")
-      
         ctx.a_usdc_agent_balance_before_in_source_chain = BalanceHelper.get_atoken_agent_balance(ctx.web3_source, ctx.a_token_address_on_source_chain)
 
         if ctx.a_usdc_agent_balance_before_in_source_chain is None:
