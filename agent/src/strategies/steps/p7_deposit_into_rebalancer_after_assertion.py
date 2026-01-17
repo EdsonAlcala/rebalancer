e4cbd730f6d2aec4e7eb5cca1df9bf08bf61658b
edsonalcala <edson_alcala@msn.com>
2026-01-17 03:48:23 +0100

    restored code

--- agent/src/strategies/steps/p7_deposit_into_rebalancer_after_assertion.py
+++ agent/src/strategies/steps/p7_deposit_into_rebalancer_after_assertion.py
@@ -7,6 +7,5 @@ class DepositIntoRebalancerAfterAssertion(Step):
 
     async def run(self, ctx: StrategyContext):
         print("Depositing into rebalancer after assertion")
-        raise ValueError("Testing 1,2,3...")
 
         # TODO: I should assert that the balance after deposit is what I had before - deposited amount
